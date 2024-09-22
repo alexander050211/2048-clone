78d55ce17771242bf69def28147a3fa376a0d096
alexander050211 <70711686+alexander050211@users.noreply.github.com>
2024-09-22 21:42:26 +0900

    Revert "Actual game and logic"

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -2,14 +2,8 @@ type TileProps = {
   value: number;
 };
 
-function getTileClass(value: number): string {
-  if (value === 0) return 'tile tile-empty';
-  return 'tile tile-' + value.toString();
-}
-
 function Tile({ value }: TileProps) {
-  const tileClass = getTileClass(value);
-  return <div className={tileClass}>{value !== 0 ? value : ''}</div>;
+  return <div className="tile">{value !== 0 ? value : ''}</div>;
 }
 
 export default Tile;
