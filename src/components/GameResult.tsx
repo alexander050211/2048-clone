9e2a2ed43fdeab8dc6b2071728c2b798b5d66950
alexander050211 <70711686+alexander050211@users.noreply.github.com>
2024-11-25 17:19:33 +0900

    Reapply "Merge pull request #10 from alexander050211/revert-9-refactoring"
    This reverts commit 2b4b5e5b07b3baf41dcd95eef798b72c519993be.

--- src/components/GameResult.tsx
+++ src/components/GameResult.tsx
@@ -1,35 +1,18 @@
 import type { GameStatus } from '../types';
-import styles from './GameResult.module.css';
 
 type GameResultProps = {
   result: GameStatus;
-  continues: boolean;
   onRestart: () => void;
-  onContinue: () => void;
 };
 
-function GameResult({
-  result,
-  continues,
-  onRestart,
-  onContinue,
-}: GameResultProps) {
+function GameResult({ result, onRestart }: GameResultProps) {
   const message = result.toString() === 'win' ? 'You Win!' : 'Game Over';
 
   return (
-    <div className={styles.gameResult}>
-      <div className={styles.overlayContent}>
+    <div className="game-result">
+      <div className="overlay-content">
         <h1>{message}</h1>
-        <div className={styles.buttonGroup}>
-          {result === 'win' && !continues && (
-            <button onClick={onContinue} className={styles.continueButton}>
-              Keep Playing
-            </button>
-          )}
-          <button onClick={onRestart} className={styles.restartButton}>
-            New Game
-          </button>
-        </div>
+        <button onClick={onRestart}>Restart?</button>
       </div>
     </div>
   );
--- src/components/Restart.tsx
+++ src/components/Restart.tsx
@@ -1,12 +1,10 @@
-import styles from './Restart.module.css';
-
 type RestartProps = {
   onRestart: () => void;
 };
 
 function Restart({ onRestart }: RestartProps) {
   return (
-    <div className={styles.restart}>
+    <div className="restart">
       <button onClick={onRestart}>New Game</button>
     </div>
   );
